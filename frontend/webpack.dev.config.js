6e70804f17cd9a4a3cd3ae35a5cd1ea2307df033
Dennis Schmidt <schmidt.dennis@online.de>
2016-10-06 14:00:15 +0200

    cleanup webpack configs

--- frontend/webpack.dev.config.js
+++ frontend/webpack.dev.config.js
@@ -3,19 +3,15 @@ var path = require('path')
 var autoprefixer = require('autoprefixer')
 var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
-var limitlessDir = path.resolve(__dirname, './vendor/limitless')
-var imagesDir = path.resolve(__dirname, './src/login/images')
-
-module.exports = {
+module.exports = [{
   entry: {
-    login: "./src/login/index.js",
     app: [
       `webpack-dev-server/client?http://${process.env.HOST}:${process.env.PORT}`,
       'webpack/hot/only-dev-server',
       'react-hot-loader/patch',
       './src/app/index.js'
     ],
-    graphiql: './src/graphiql'
+    vendor: ["react", "react-dom", "react-router", "moment", "react-apollo", "apollo-client", "graphql-tag", "jquery"]
   },
 
   output: {
@@ -34,15 +30,7 @@ module.exports = {
         test: /\.js?$/,
         exclude: [ /node_modules/ ],
         loaders: [ 'babel-loader' ]
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          notExtractLoader: 'style-loader',
-          loader: 'css-loader?modules&importLoaders=1&localIdentName=[local]',
-        })
-      },
-      {
+      },{
         test: /\.sass$/,
         loader: ExtractTextPlugin.extract({
           notExtractLoader: 'style-loader',
@@ -57,7 +45,7 @@ module.exports = {
       },{
         test: /\.(png|jpg|gif)$/,
         exclude: ["node_modules", "src"],
-        loader: 'file?name=[path][name].[ext]&context=' + limitlessDir
+        loader: 'file?name=images/[path][name].[ext]&context=src'
       },{
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
         loader: "url?limit=10000&mimetype=application/font-woff"
@@ -88,6 +76,103 @@ module.exports = {
       jQuery: "jquery",
       "window.jQuery": "jquery"
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: '[name].js'
+    })
   ]
-}
+},{
+  entry: {
+    login: "./src/login/index.js"
+  },
+  output: {
+    publicPath: `http://${process.env.HOST}:${process.env.PORT}/assets/`,
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract({
+          notExtractLoader: 'style-loader',
+          loader: 'css-loader?modules&importLoaders=1&localIdentName=[local]!resolve-url!sass-loader?sourceMap',
+        })
+      },{
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract({
+          notExtractLoader: 'style-loader',
+          loader: 'css-loader?modules&importLoaders=1&localIdentName=[local]!less-loader',
+        })
+      },{
+        test: /\.(png|jpg|gif)$/,
+        exclude: ["node_modules"],
+        loader: 'file?name=images/[path][name].[ext]&context=src'
+      },{
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },{
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },{
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },{
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      },{
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        exclude: /images/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      allChunks: true
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    })
+  ]
+},{
+  entry: {
+    graphiql: './src/graphiql'
+  },
+
+  output: {
+    publicPath: `http://${process.env.HOST}:${process.env.PORT}/assets/`,
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: [ /node_modules/ ],
+        loaders: [ 'babel-loader' ]
+      },{
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          notExtractLoader: 'style-loader',
+          loader: 'css-loader?modules&importLoaders=1&localIdentName=[local]',
+        })
+      },{
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract({
+          notExtractLoader: 'style-loader',
+          loader: 'css-loader?modules&importLoaders=1&localIdentName=[local]!sass-loader',
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      allChunks: true
+    })
+  ]
+}]
